9bad58256812225bc2b1423cad732ced06e2720a
Willians Silveira <it.willians@gmail.com>
2020-02-09 23:03:10 -0300

    Ajuste base uri

--- features/pages/startup_page.rb
+++ features/pages/startup_page.rb
@@ -1,7 +1,7 @@
 class Startup
     include HTTParty
     require_relative '../hooks/startup_hook'
-    base_uri "http://5b49f9b0ff11b100149bf42b.mockapi.io/cm"
+    base_uri "http://5b49f9b0ff11b100149bf42b.mockapi.ioo/cm"
      
     def initialize(body)
       @options = {:body => body}
